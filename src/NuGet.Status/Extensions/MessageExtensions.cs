db6b72fa7e956982dff31855bd72246942e6e124
Scott Bommarito <scottbom@microsoft.com>
2019-03-20 17:30:22 -0700

    switch to markdig

--- src/NuGet.Status/Extensions/MessageExtensions.cs
+++ src/NuGet.Status/Extensions/MessageExtensions.cs
@@ -1,4 +1,4 @@
-﻿using MarkdownDeep;
+﻿using Markdig;
 using NuGet.Services.Status;
 
 namespace NuGet.Status.Extensions
@@ -8,16 +8,11 @@ namespace NuGet.Status.Extensions
         /// <summary>
         /// Converts the <see cref="Message.Contents"/> of <paramref name="message"/> to HTML.
         /// </summary>
-        /// <remarks>
-        /// The <see cref="Markdown"/> instance is NOT thread-safe. It must be created on every request.
-        /// </remarks>
         public static string GetHtmlContents(this Message message)
         {
-            return new Markdown
-            {
-                SafeMode = true,
-                ExtraMode = false
-            }.Transform(message.Contents);
+            var pipelineBuilder = new MarkdownPipelineBuilder();
+            pipelineBuilder.DisableHtml();
+            return Markdown.ToHtml(message.Contents, pipelineBuilder.Build());
         }
     }
 }
